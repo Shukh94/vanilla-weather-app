61c14a347522e6746a92c9d3cdd277d213983cbb
Shukh94 <shakila.sk09@gmail.com>
2023-10-17 00:31:36 +0600

    added relevant icon from the api

--- js/index.js
+++ js/index.js
@@ -27,6 +27,9 @@ function showTemp(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  document.querySelector("#icon").setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description );
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;

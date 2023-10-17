95502eefea804a6669337db6bc2ab1050c23773f
Shukh94 <shakila.sk09@gmail.com>
2023-10-18 00:34:23 +0600

    added celsius, fahrenheit js and css class

--- js/index.js
+++ js/index.js
@@ -16,10 +16,10 @@ function formatDate(timestamp) {
 }
 
 function showTemp(response) {
+  celsiusTemperature = response.data.main.temp;
+
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -33,7 +33,9 @@ function showTemp(response) {
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+
 }
+
 
 function search(city) {
 let apiKey = "f7af430b9dd12626ceea8cea065114b4";
@@ -47,7 +49,34 @@ function hamdleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("Paris")
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature")
+  let fahrenheitTemperature = (celsiusTemperature * 9) /5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature")
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null ;
+
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", hamdleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature)
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature)
+
+
+search("Paris")

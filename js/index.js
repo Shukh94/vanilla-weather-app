40eb4e907e0f08b3aa39c8aaf01bc5b4441d817a
Shukh94 <108126378+Shukh94@users.noreply.github.com>
2023-10-18 17:26:08 +0600

    Redo previous one

--- js/index.js
+++ js/index.js
@@ -15,33 +15,6 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHtml = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-                <div class="col-2">
-                  <div class="weather-forecast-date">${day}</div>
-                  <img
-                    src="http://openweathermap.org/img/wn/50d@2x.png"
-                    alt=""
-                    width="42"
-                  />
-                  <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperature-max"> 18° </span>
-                    <span class="weather-forecast-temperature-min"> 12° </span>
-                  </div>
-                </div>`;
-  });
-
-  forecastHtml = forecastHtml + `</div>`;
-  forecastElement.innerHTML = forecastHtml;
-}
-
 function showTemp(response) {
   celsiusTemperature = response.data.main.temp;
 
@@ -111,4 +84,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Paris");
-displayForecast();
